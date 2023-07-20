d4400b71eb201b5c5145bcee9c4fadf3ee406df4
Andrew H <ahuggins1235@gmail.com>
2023-07-20 15:57:02 +1000

    Initial Commit

---
+++ RideTrackr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RideTrackr
+//
+//  Created by Andrew Huggins on 20/7/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RideTrackr/RideTrackrApp.swift
@@ -0,0 +1,17 @@
+//
+//  RideTrackrApp.swift
+//  RideTrackr
+//
+//  Created by Andrew Huggins on 20/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct RideTrackrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
